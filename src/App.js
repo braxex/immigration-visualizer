e7f716247f7afe4b0a21f5a3e7a1932562082871
braxtonex <braxton.excell@gmail.com>
2017-11-16 21:14:20 -0600

    Further React Commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+
+//const ChooserButtons = () => <div>
+//                  <input name='lprOrNi' type="radio"></input>
+//                  <input name='lprOrNi' type="radio"></input>
+//                </div>
+
+//const ChooserButtons = () => (
+//  <div>
+//                  <input name='lprOrNi' type="radio"></input>
+//                  <input name='lprOrNi' type="radio"></input>
+//                </div>
+//)
+
+//const ChooserButtons = (props) => (
+//  <div>
+//                  <input name='lprOrNi' type="radio"></input>
+//                  <input name='lprOrNi' type="radio"></input>
+//                </div>
+//)
+
+//const ChooserButtons = () => {
+//  return <div>
+//                  <input name='lprOrNi' type="radio"></input>
+//                  <input name='lprOrNi' type="radio"></input>
+//                </div>
+//}
+
+function ChooserButtons() {
+  return <div>
+          <input name='lprOrNi' type="radio" ></input>
+          <input name='lprOrNi' type="radio"></input>
+         </div>
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Immigration Visualizer</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         {React.createElement('p', null, 'hi there')}
-        <div>
-          <input name='lprOrNi' type="radio"></input>
-          <input name='lprOrNi' type="radio"></input>
-        </div>
+        <ChooserButtons/>
       </div>
     );
+  }
+
+  componentWillReceiveProps() {
+
+  }
+
+  makeItGreen() {
+
   }
 }
 
